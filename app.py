afd102ded4b50ee6481fdfe13ad56120157521c8
Barra <barra@Cesar-MBA-2.local>
2024-06-30 20:17:49 -0600

    initial commit

--- app.py
+++ app.py
@@ -5,3 +5,9 @@ import numpy as np
 
 df = pd.read_csv('vehicles_us.csv')
 print(df.head())
+
+# Título de la aplicación
+st.title('Lista de Vehículos')
+
+# Mostrar el DataFrame en la aplicación
+st.write(df)
