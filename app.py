72c873f8a6410a43688a7fb72ef76662c2f2315a
Barra <barra@Cesar-MBA-2.local>
2024-06-30 20:26:51 -0600

    initial commit

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import pandas as pd
-import streamlit as st  # Importa streamlit como st
+import streamlit as st
+import plotly.express as px
 
 # Leer el archivo CSV
 df = pd.read_csv('vehicles_us.csv')
@@ -12,3 +13,13 @@ st.title('Lista de Vehículos')
 
 # Mostrar el DataFrame en la aplicación
 st.write(df)
+
+# Selección de variables para el gráfico de dispersión
+st.header('Generar Gráfico de Dispersión')
+x_axis = st.selectbox('Selecciona la variable para el eje X', df.columns)
+y_axis = st.selectbox('Selecciona la variable para el eje Y', df.columns)
+
+# Botón para generar el gráfico
+if st.button('Graficar'):
+    fig = px.scatter(df, x=x_axis, y=y_axis, title=f'Dispersión de {x_axis} vs {y_axis}')
+    st.plotly_chart(fig)
